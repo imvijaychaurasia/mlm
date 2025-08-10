5c83c7db3cd3750342c427b6c54f3cbb31554160
imvijaychaurasia <imvijaychaurasia@gmail.com>
2025-08-10 19:09:43 +0530

    build push

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mlm/',
+  base: '/mlm/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
